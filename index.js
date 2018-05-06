07143407d36263511f2edab155fa9ede323266cb
Rosco Kalis <roscokalis@gmail.com>
2018-05-06 23:41:58 +0200

    Added custom assertion message support
    - Added optional custom assertion message support
    - Improved regular assertion messages, providing both custom and default when a custom one is provided
    - Removed dependency on Chai, and instead throw AssertionErrors through the assertion-error dependency
    - Added some commentary

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
 const _ = require('lodash');
-const assert = require('chai').assert;
+const AssertionError = require('assertion-error');
+
+/* Creates a new assertion message, containing the passedAssertionMessage and
+ * the defaultAssertion message when passedAssertionMessage exists, otherwise
+ * just the default.
+ */
+createAssertionMessage = (passedMessage, defaultMessage) => {
+  let assertionMessage = defaultMessage;
+  if (passedMessage) {
+    assertionMessage = `${passedMessage} : ${defaultMessage}`;
+  }
+  return assertionMessage
+}
+
+assertEventListNotEmpty = (list, passedMessage, defaultMessage) => {
+  const assertionMessage = createAssertionMessage(passedAssertionMessage, defaultMessage);
+  if (!Array.isArray(list) || list.length === 0) {
+    throw new AssertionError(assertionMessage);
+  }
+}
+
+assertEventListEmpty = (list, passedMessage, defaultMessage) => {
+  const assertionMessage = createAssertionMessage(passedMessage, defaultMessage);
+  if (Array.isArray(list) && list.length !== 0) {
+    throw new AssertionError(assertionMessage);
+  }
+}
 
 module.exports = {
-  eventEmitted: (result, eventType, filter) => {
+  eventEmitted: (result, eventType, filter, message) => {
+    /* Filter correct event types */
     const events = _.filter(result.logs, (entry) => {
       return entry.event === eventType;
     });
-    assert.isNotEmpty(events, `Event of type ${eventType} was not emitted`);
+    assertEventListNotEmpty(events, message, `Event of type ${eventType} was not emitted`);
 
-    if (filter === null) {
-        return;
+    /* Return if no filter function was provided */
+    if (filter === undefined || filter === null) {
+      return;
     }
 
+    /* Filter correct arguments */
     let eventArgs = _.map(events, (entry) => {
-        return entry.args;
+      return entry.args;
     });
     eventArgs = _.filter(eventArgs, filter);
-    assert.isNotEmpty(eventArgs, `Event filter for ${eventType} returned no results`);
+    assertEventListNotEmpty(eventArgs, message, `Event filter for ${eventType} returned no results`);
   },
-  eventNotEmitted: (result, eventType, filter) => {
+  eventNotEmitted: (result, eventType, filter, message) => {
+    /* Filter correct event types */
     const events = _.filter(result.logs, (entry) => {
-        return entry.event === eventType;
+      return entry.event === eventType;
     });
 
-    if (filter === null) {
-        assert.isEmpty(events, `Event of type ${eventType} was emitted`);
-        return;
+    /* Only check filtered events if there is no provided filter function */
+    if (filter == undefined || filter === null) {
+      assertEventListEmpty(events, message, `Event of type ${eventType} was emitted`);
+      return;
     }
 
+    /* Filter correct arguments */
     let eventArgs = _.map(events, (entry) => {
-        return entry.args;
+      return entry.args;
     });
     eventArgs = _.filter(eventArgs, filter);
-    assert.isEmpty(eventArgs, `Event filter for ${eventType} returned results`);
+    assertEventListEmpty(eventArgs, message, `Event filter for ${eventType} returned results`);
   }
 }
