fc846c17d750f9171514245785feac0e7f701a62
Rosco Kalis <roscokalis@gmail.com>
2018-06-17 12:03:35 +0200

    Improved default error messages (#2)
    - Added a list of emitted events with their parameters to the
    default error message on test failure.
    - Added an exported prettyPrintEmittedEvents function that
    pretty prints all events emitted in a certain transaction.
    - These lists work with indexed parameters as well, contrary to the
    default truffle implementation.

--- index.js
+++ index.js
@@ -27,13 +27,40 @@ assertEventListEmpty = (list, passedMessage, defaultMessage) => {
   }
 }
 
+/* Returns event string in the form of EventType(arg1, arg2, ...) */
+getPrettyEventString = (eventType, args) => {
+  let argString = '';
+  Object.entries(args).forEach(([key, value]) => {
+    argString += `, ${key}: ${value}`;
+  });
+  argString = argString.replace(', ', '');
+  return `${eventType}(${argString})`;
+}
+
+/* Returns a list of all emitted events in a transaction,
+ * using the format of getPrettyEventString
+ */
+getPrettyEmittedEventsString = (result) => {
+  if (result.logs.length === 0) {
+    return `    No events emitted in tx ${result.tx}\n`;
+  }
+  let string = `    Events emitted in tx ${result.tx}:\n`;
+  string += `    ----------------------------------------------------------------------------------------\n`;
+  for (const emittedEvent of result.logs) {
+    string += `    ${getPrettyEventString(emittedEvent.event, emittedEvent.args)}\n`;
+  }
+  string += `    ----------------------------------------------------------------------------------------\n`;
+  return string;
+}
+
 module.exports = {
   eventEmitted: (result, eventType, filter, message) => {
     /* Filter correct event types */
     const events = _.filter(result.logs, (entry) => {
       return entry.event === eventType;
     });
-    assertEventListNotEmpty(events, message, `Event of type ${eventType} was not emitted`);
+    //TODO: Move the getPrettyEmittedEventsString to the assertion functions
+    assertEventListNotEmpty(events, message, `Event of type ${eventType} was not emitted\n${getPrettyEmittedEventsString(result)}`);
 
     /* Return if no filter function was provided */
     if (filter === undefined || filter === null) {
@@ -45,7 +72,7 @@ module.exports = {
       return entry.args;
     });
     eventArgs = _.filter(eventArgs, filter);
-    assertEventListNotEmpty(eventArgs, message, `Event filter for ${eventType} returned no results`);
+    assertEventListNotEmpty(eventArgs, message, `Event filter for ${eventType} returned no results\n${getPrettyEmittedEventsString(result)}`);
   },
   eventNotEmitted: (result, eventType, filter, message) => {
     /* Filter correct event types */
@@ -55,7 +82,7 @@ module.exports = {
 
     /* Only check filtered events if there is no provided filter function */
     if (filter == undefined || filter === null) {
-      assertEventListEmpty(events, message, `Event of type ${eventType} was emitted`);
+      assertEventListEmpty(events, message, `Event of type ${eventType} was emitted\n${getPrettyEmittedEventsString(result)}`);
       return;
     }
 
@@ -64,6 +91,9 @@ module.exports = {
       return entry.args;
     });
     eventArgs = _.filter(eventArgs, filter);
-    assertEventListEmpty(eventArgs, message, `Event filter for ${eventType} returned results`);
+    assertEventListEmpty(eventArgs, message, `Event filter for ${eventType} returned results\n${getPrettyEmittedEventsString(result)}`);
+  },
+  prettyPrintEmittedEvents: (result) => {
+    console.log(getPrettyEmittedEventsString(result));
   }
 }
