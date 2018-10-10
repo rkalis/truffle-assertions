e9fd763c3ec466eb6ff2d561138db21f3e04b8ee
Rosco Kalis <roscokalis@gmail.com>
2018-10-10 15:01:48 +0200

    Added test stubs for different functions and added travis config

---
+++ test/test.js
@@ -0,0 +1,22 @@
+const truffleAssert = require('..');
+
+describe('truffleAssert', () => {
+  describe('eventEmitted', () => {
+
+  })
+  describe('eventNotEmitted', () => {
+
+  })
+  describe('prettyPrintEmittedEvents', () => {
+
+  })
+  describe('createTransactionResult', () => {
+
+  })
+  describe('fails', () => {
+
+  })
+  describe('reverts', () => {
+
+  })
+})
