a8fc0b22a9e8d57dd5e6240eccdcd24b07c9acc5
Zachary Nawar <zanawar@microsoft.com>
2018-10-16 17:17:43 -0700

    Updates for linting requirements.

--- index.js
+++ index.js
@@ -118,7 +118,6 @@ const createTransactionResult = async (contract, transactionHash) => {
 const fails = async (asyncFn, errorType, reason, message) => {
   try {
     await asyncFn;
-    
   } catch (error) {
     if (errorType && !error.message.includes(errorType)) {
       const assertionMessage = createAssertionMessage(message, `Expected to fail with ${errorType}, but failed with: ${error}`);
@@ -127,11 +126,9 @@ const fails = async (asyncFn, errorType, reason, message) => {
       const assertionMessage = createAssertionMessage(message, `Expected to fail with ${reason}, but failed with: ${error}`);
       throw new AssertionError(assertionMessage);
     }
-
     // Eror was handled by errorType or reason
     return;
   }
-
   const assertionMessage = createAssertionMessage(message, 'Did not fail');
   throw new AssertionError(assertionMessage);
 };
