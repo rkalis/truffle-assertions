3c8a1327a2cf8b4c81b316cb04486efd68ac4b10
Rosco Kalis <roscokalis@gmail.com>
2018-12-05 22:47:09 +0100

    Added tests for eventNotEmitted

--- test/main.test.js
+++ test/main.test.js
@@ -6,7 +6,7 @@ const winResult = require('./fixture/winResult.json');
 
 describe('truffleAssert', () => {
   describe('eventEmitted', () => {
-    it('should fail when event with is not emitted', () => {
+    it('should fail when event is not emitted', () => {
       assert.throws(() => truffleAssert.eventEmitted(loseResult, 'Payout'), AssertionError);
     });
 
@@ -36,7 +36,35 @@ describe('truffleAssert', () => {
   });
 
   describe('eventNotEmitted', () => {
+    it('should fail when event is emitted', () => {
+      assert.throws(() => truffleAssert.eventNotEmitted(loseResult, 'Play'), AssertionError);
+    });
+
+    it('should return passed error message when event is emitted', () => {
+      assert.throws(() => truffleAssert.eventNotEmitted(loseResult, 'Play', null, 'Should not play'), /Should not play/);
+    });
+
+    it('should fail when event with specified arguments is emitted', () => {
+      assert.throws(() => (
+        truffleAssert.eventNotEmitted(winResult, 'Play', ev => ev.betNumber === ev.winningNumber)
+      ), AssertionError);
+    });
+
+    it('should return passed error message when event with specified arguments is emitted', () => {
+      assert.throws(() => (
+        truffleAssert.eventNotEmitted(winResult, 'Play', ev => ev.betNumber === ev.winningNumber, 'Should not win')
+      ), /Should not win/);
+    });
+
+    it('should pass when event is not emitted', () => {
+      truffleAssert.eventNotEmitted(loseResult, 'Payout');
+    });
+
+    it('should pass when event with specified arguments is not emitted', () => {
+      truffleAssert.eventNotEmitted(winResult, 'Play', ev => ev.betNumber !== ev.winningNumber);
+    });
   });
+
   describe('prettyPrintEmittedEvents', () => {
   });
   describe('createTransactionResult', () => {
