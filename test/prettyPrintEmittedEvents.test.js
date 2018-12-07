666373565ee3e7561347b230a864d986e021b792
Rosco Kalis <roscokalis@gmail.com>
2018-12-07 19:46:44 +0100

    Added tests for prettyPrintEmittedEvents

--- test/prettyPrintEmittedEvents.test.js
+++ test/prettyPrintEmittedEvents.test.js
@@ -1,8 +1,35 @@
 const { assert } = require('chai');
+const { stub } = require('sinon');
 const truffleAssert = require('..');
 
+const truffleV4winResult = require('./fixture/truffleV4winResult.json');
+
+// TODO: Truffle v5 -> don't print { 0: xxx, 1: xxx }
 describe('prettyPrintEmittedEvents', () => {
-  it('should initialise', () => {
-    assert.isDefined(truffleAssert);
+  it('should print all emitted events', () => {
+    const logStub = stub(console, 'log');
+
+    truffleAssert.prettyPrintEmittedEvents(truffleV4winResult);
+
+    assert(logStub.calledWithMatch('Play'));
+    assert(logStub.calledWithMatch('Payout'));
+    assert(logStub.calledWithMatch(truffleV4winResult.tx));
+    assert(logStub.neverCalledWithMatch('No events'));
+
+    logStub.restore();
+  });
+
+  it('should print no events when no events were emitted', () => {
+    const logStub = stub(console, 'log');
+
+    truffleV4winResult.logs = [];
+    truffleAssert.prettyPrintEmittedEvents(truffleV4winResult);
+
+    assert(logStub.neverCalledWithMatch('Play'));
+    assert(logStub.neverCalledWithMatch('Payout'));
+    assert(logStub.calledWithMatch(truffleV4winResult.tx));
+    assert(logStub.calledWithMatch('No events'));
+
+    logStub.restore();
   });
 });
