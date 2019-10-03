cf9c1ad5ba5bfbbcb70b8ba36e0eacf90fc9eb4a
Rosco Kalis <roscokalis@gmail.com>
2019-10-03 17:07:59 +0900

    Add deployment details to docs/kalis-me-tutorial-code

---
+++ docs/kalis-me-tutorial-code/truffle-config.js
@@ -0,0 +1,31 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
+      },
+      network_id: 3
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`)
+      },
+      network_id: 4
+    },
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://goerli.infura.io/v3/${process.env.INFURA_ID}`)
+      },
+      network_id: 5,
+      gas: 6500000
+    }
+  }
+};
